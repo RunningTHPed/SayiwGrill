5697d899d62e01b3d9bbd8b57ddd177b40a90448
n1ghtmare57 <bcc34868@hotmail.com>
2019-10-24 22:01:36 +0700

    pop up  draft

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,29 +1,53 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class   MainActivity extends AppCompatActivity {
 
     int number = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("fuck")
+                .setMessage("Message")
+                .setPositiveButton("ok",null)
+                .setNegativeButton("cancel",null)
+                .show();
+
+
+
+
+
 
         Button Button1 = (Button) findViewById(R.id.button1);
         Button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                Button positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                positiveButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this,"Not closing",Toast.LENGTH_SHORT).show();
+
+                    }
+                });
                 startActivity(new Intent(MainActivity.this, Menu.class));
                 number = 1;
                 Intent intent = new Intent(MainActivity.this, Menu.class);
                 intent.putExtra("MyX", number);
                 startActivity(intent);
+
             }
         });
 
