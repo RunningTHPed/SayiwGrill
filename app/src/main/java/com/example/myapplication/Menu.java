15eb07af9cd60d4454b82ad7dd0f2f591c4951ef
Dio sama <bcc34868@hotmail.com>
2019-10-24 23:32:06 +0700

    Update Menu.java

--- app/src/main/java/com/example/myapplication/Menu.java
+++ app/src/main/java/com/example/myapplication/Menu.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -46,7 +47,8 @@ public class Menu extends AppCompatActivity {
         Button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
+                Intent i =new Intent(Menu.this,MainActivity.class);
+                startActivity(i);
             }
         });
 
