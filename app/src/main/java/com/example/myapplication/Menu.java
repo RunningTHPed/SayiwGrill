c7b7cb1c6450e662089e42e4ced264b5bf708228
n1ghtmare57 <bcc34868@hotmail.com>
2019-10-24 15:22:48 +0700

    dbug back button

--- app/src/main/java/com/example/myapplication/Menu.java
+++ app/src/main/java/com/example/myapplication/Menu.java
@@ -4,10 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class Menu extends AppCompatActivity {
+
+    private long backPressedTime;
+    private Toast backToast;
+
     @Override
+    public void onBackPressed() {
+        if (backPressedTime + 0.01 > System.currentTimeMillis()) {
+            backToast.cancel();
+            super.onBackPressed();
+            return;
+        } else {
+            backToast = Toast.makeText(getBaseContext(), "Don't Press Button", Toast.LENGTH_SHORT);
+            backToast.show();
+        }
+
+        backPressedTime = System.currentTimeMillis();
+    }
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
@@ -18,5 +36,7 @@ public class Menu extends AppCompatActivity {
 
        TextView text = (TextView)findViewById(R.id.textView3);
        text.setText("people :"+number);
+
+
     }
 }
