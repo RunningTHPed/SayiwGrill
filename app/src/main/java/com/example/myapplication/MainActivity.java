88c5cc476ddc5e0145a2bdf76e2ee93cfa5027c4
Dio <56954260+lLunaryl@users.noreply.github.com>
2019-11-19 20:21:50 +0700

    eiei

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,28 +1,32 @@
 package com.example.myapplication;
-
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
 
 
 public class   MainActivity extends AppCompatActivity {
 
 
     int number = 0;
+    Dialog myDialog;
 
-
-    public void popup(){
+    public void popup() {
 
         AlertDialog dialog = new AlertDialog.Builder(this)
                 .setTitle("")
-                .setMessage("คุณมา"+number+"คน?")
-                .setPositiveButton("ใช่",null)
-                .setNegativeButton("ไม่ใช่",null)
+                .setMessage("คุณมา" + number + "คน?")
+                .setPositiveButton("ใช่", null)
+                .setNegativeButton("ไม่ใช่", null)
                 .show();
 
         Button positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
@@ -39,7 +43,6 @@ public class   MainActivity extends AppCompatActivity {
     }
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,22 +50,19 @@ public class   MainActivity extends AppCompatActivity {
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // LANDSCAPE ONLY
 
 
+        myDialog = new Dialog(this);
 
 
-
-
-
-
-
-        Button Button1 = (Button) findViewById(R.id.button1);
+        /*Button Button1 = (Button) findViewById(R.id.button1);
         Button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 number = 1;
-                popup();
+                //popup();
+                //ShowPopup();
 
             }
-        });
+        });*/
 
         Button Button2 = (Button) findViewById(R.id.button2);
         Button2.setOnClickListener(new View.OnClickListener() {
@@ -91,4 +91,92 @@ public class   MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    public void ShowPopup1(View v) {
+        TextView txtclose;
+        Button btnFollow;
+        myDialog.setContentView(R.layout.custompopup);
+        txtclose = (TextView) myDialog.findViewById(R.id.txtclose);
+        txtclose.setText("NO");
+        TextView textVideoName = myDialog.findViewById(R.id.textVideoName);
+        textVideoName.setText("คุ");
+        btnFollow = (Button) myDialog.findViewById(R.id.btnfollow);
+        txtclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myDialog.dismiss();
+            }
+        });
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        myDialog.show();
+
+
+    }
+    public void ShowPopup2(View v) {
+        TextView txtclose;
+        Button btnFollow;
+        myDialog.setContentView(R.layout.custompopup);
+        txtclose = (TextView) myDialog.findViewById(R.id.txtclose);
+        txtclose.setText("NO");
+        TextView textVideoName = myDialog.findViewById(R.id.textVideoName);
+        textVideoName.setText("คุ");
+        btnFollow = (Button) myDialog.findViewById(R.id.btnfollow);
+        txtclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myDialog.dismiss();
+            }
+        });
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        myDialog.show();
+
+
+    }
+    public void ShowPopup3(View v) {
+        TextView txtclose;
+        Button btnFollow;
+        myDialog.setContentView(R.layout.custompopup);
+        txtclose = (TextView) myDialog.findViewById(R.id.txtclose);
+        txtclose.setText("NO");
+        TextView textVideoName = myDialog.findViewById(R.id.textVideoName);
+        textVideoName.setText("คุ");
+        btnFollow = (Button) myDialog.findViewById(R.id.btnfollow);
+        txtclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myDialog.dismiss();
+            }
+        });
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        myDialog.show();
+
+
+    }
+    public void ShowPopup4(View v) {
+        TextView txtclose;
+        Button btnFollow;
+        myDialog.setContentView(R.layout.custompopup);
+        txtclose = (TextView) myDialog.findViewById(R.id.txtclose);
+        txtclose.setText("NO");
+        TextView textVideoName = myDialog.findViewById(R.id.textVideoName);
+        textVideoName.setText("คุ");
+        btnFollow = (Button) myDialog.findViewById(R.id.btnfollow);
+        txtclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myDialog.dismiss();
+            }
+        });
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        myDialog.show();
+
+
+    }
+
+
 }
